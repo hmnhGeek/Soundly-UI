856135d807fd72796b6a406db28315a61637d800
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-24 20:40:41 +0530

    restored Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,6 @@ import {
   IconButton,
   TextField,
   Box,
-  Avatar,
 } from "@mui/material";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import PauseIcon from "@mui/icons-material/Pause";
@@ -23,7 +22,7 @@ import EditCoverImageModal from "./EditCoverImageModal";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const { songs, auth, userProfileImage } = useContext(AuthContext);
+  const { songs, auth } = useContext(AuthContext);
   const [searchTerm, setSearchTerm] = useState("");
   const [currentSong, setCurrentSong] = useState(null);
   const [isPlaying, setIsPlaying] = useState(false);
@@ -74,135 +73,114 @@ const Home = () => {
   };
 
   return (
-    <>
-      <Box
-        sx={{
-          maxWidth: 1200,
-          margin: "auto",
-          mt: 5,
-          display: "flex", // Arrange elements side-by-side
-          gap: 2, // Adds spacing between song list and player
-        }}
-      >
-        {/* Left side: Song List */}
-        <Box sx={{ flex: 1, width: currentSong ? "50%" : "100%" }}>
-          <Box
-            sx={{
-              maxWidth: 1200,
-              margin: "auto",
-              mb: 5,
-              display: "flex", // Arrange elements side-by-side
-              gap: 2, // Adds spacing between song list and player
-              // width: currentSong ? "50%" : "100%",
-            }}
-          >
-            <img
-              src={userProfileImage}
-              alt="Profile"
-              width="150"
-              height="150"
-            />
-          </Box>
-          <br />
-          <TextField
-            label="Search Songs"
-            variant="outlined"
-            fullWidth
-            sx={{ mb: 2 }}
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
+    <Box
+      sx={{
+        maxWidth: 1200,
+        margin: "auto",
+        mt: 5,
+        display: "flex", // Arrange elements side-by-side
+        gap: 2, // Adds spacing between song list and player
+      }}
+    >
+      {/* Left side: Song List */}
+      <Box sx={{ flex: 1, width: currentSong ? "50%" : "100%" }}>
+        <TextField
+          label="Search Songs"
+          variant="outlined"
+          fullWidth
+          sx={{ mb: 2 }}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
 
-          <TableContainer
-            component={Paper}
-            sx={{
-              borderRadius: 2,
-              // mb: 2,
-              boxShadow: 3,
-              maxHeight: 390,
-              overflowY: "auto",
-              scrollbarWidth: "none", // Firefox
-              "&::-webkit-scrollbar": { display: "none" }, // Chrome, Safari, Edge
-            }}
-          >
-            <Table stickyHeader>
-              <TableHead>
-                <TableRow>
-                  <TableCell></TableCell>
+        <TableContainer
+          component={Paper}
+          sx={{
+            borderRadius: 2,
+            boxShadow: 3,
+            maxHeight: 600,
+            overflowY: "auto",
+            scrollbarWidth: "none", // Firefox
+            "&::-webkit-scrollbar": { display: "none" }, // Chrome, Safari, Edge
+          }}
+        >
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell>
+                  <strong>Song</strong>
+                </TableCell>
+                <TableCell>
+                  <strong>Change Cover</strong>
+                </TableCell>
+                <TableCell>
+                  <strong>Delete</strong>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredSongs.map((song) => (
+                <TableRow key={song.id}>
                   <TableCell>
-                    <strong>Song</strong>
+                    <IconButton
+                      color="primary"
+                      onClick={() => handlePlayPause(song)}
+                    >
+                      {currentSong?.id === song.id && isPlaying ? (
+                        <PauseIcon />
+                      ) : (
+                        <PlayArrowIcon />
+                      )}
+                    </IconButton>
+                  </TableCell>
+                  <TableCell>{song.originalName}</TableCell>
+                  <TableCell>
+                    <IconButton
+                      color="secondary"
+                      onClick={() => initiateCoverImageChange(song.id)}
+                    >
+                      <Image />
+                    </IconButton>
                   </TableCell>
                   <TableCell>
-                    <strong>Change Cover</strong>
-                  </TableCell>
-                  <TableCell>
-                    <strong>Delete</strong>
+                    <IconButton
+                      color="warning"
+                      onClick={() => initiateDelete(song)}
+                    >
+                      <Delete />
+                    </IconButton>
                   </TableCell>
                 </TableRow>
-              </TableHead>
-              <TableBody>
-                {filteredSongs.map((song) => (
-                  <TableRow key={song.id}>
-                    <TableCell>
-                      <IconButton
-                        color="primary"
-                        onClick={() => handlePlayPause(song)}
-                      >
-                        {currentSong?.id === song.id && isPlaying ? (
-                          <PauseIcon />
-                        ) : (
-                          <PlayArrowIcon />
-                        )}
-                      </IconButton>
-                    </TableCell>
-                    <TableCell>{song.originalName}</TableCell>
-                    <TableCell>
-                      <IconButton
-                        color="secondary"
-                        onClick={() => initiateCoverImageChange(song.id)}
-                      >
-                        <Image />
-                      </IconButton>
-                    </TableCell>
-                    <TableCell>
-                      <IconButton
-                        color="warning"
-                        onClick={() => initiateDelete(song)}
-                      >
-                        <Delete />
-                      </IconButton>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Box>
-
-        {/* Right side: Song Player (only visible when a song is selected) */}
-        {currentSong && (
-          <Box sx={{ flex: 1, width: "50%" }}>
-            <SongPlayer
-              song={currentSong}
-              setSong={setCurrentSong}
-              isPlaying={isPlaying}
-              onMusicEnd={playNextMusic}
-            />
-          </Box>
-        )}
-
-        <DeleteMusicDialog
-          state={deleteDialogState}
-          setter={setDeleteDialogState}
-        />
-
-        <EditCoverImageModal
-          songId={coverImageModalState.songId}
-          isVisible={coverImageModalState.show}
-          onClose={() => setCoverImageModalState({ show: false, songId: null })}
-        />
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
-    </>
+
+      {/* Right side: Song Player (only visible when a song is selected) */}
+      {currentSong && (
+        <Box sx={{ flex: 1, width: "50%" }}>
+          <SongPlayer
+            song={currentSong}
+            setSong={setCurrentSong}
+            isPlaying={isPlaying}
+            onMusicEnd={playNextMusic}
+          />
+        </Box>
+      )}
+
+      <DeleteMusicDialog
+        state={deleteDialogState}
+        setter={setDeleteDialogState}
+      />
+
+      <EditCoverImageModal
+        songId={coverImageModalState.songId}
+        isVisible={coverImageModalState.show}
+        onClose={() => setCoverImageModalState({ show: false, songId: null })}
+      />
+    </Box>
   );
 };
 
